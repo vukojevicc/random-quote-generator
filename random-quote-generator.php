d79b552913ca6872bb8977cd39b86b34e98ce3c1
Aleksandar <aleksandar.vukojevicc@gmail.com>
2023-03-13 21:27:49 +0100

    initial commit

---
+++ random-quote-generator.php
@@ -0,0 +1,9 @@
+/*
+Plugin Name: Random Quote Admin Dashboard
+Plugin URI: https://yourpluginurl.com/
+Description: Displays a random quote on the WordPress admin dashboard
+Version: 1.0
+Author: Aleksandar Vukojevic
+Author URI: https://aleksandarv.com/
+License: GPL2
+*/
