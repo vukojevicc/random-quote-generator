be300ad74137108cd334a6009f9efb993ccf1bd9
Aleksandar <aleksandar.vukojevicc@gmail.com>
2023-03-14 17:45:32 +0100

    final commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let quote
+(async function getRandomQuote() {
+  const response = await fetch("https://api.quotable.io/random")
+  const data = await response.json()
+  const quote = data.content
+  const author = data.author
+  const quoteDiv = document.querySelector('#random-quote-generator-div')
 
-fetch("https://api.quotable.io/random")
-  .then((response) => response.json())
-  .then((data) => {
-    quote = data.content
-    document.querySelector('#random-quote').innerText = quote
-  });
+  quoteDiv.querySelector('blockquote').innerText = quote
+  quoteDiv.querySelector('span').innerText += author
+})()
--- random-quote-generator.php
+++ random-quote-generator.php
@@ -1,21 +1,29 @@
 <?php
 /*
-Plugin Name: Random Quote Admin Dashboard
+Plugin Name: Random Quote Generator
 Plugin URI: https://github.com/vukojevicc/random-quote-generator
 Description: Displays a random quote on the WordPress admin dashboard
 Version: 1.0
 Author: Aleksandar Vukojevic
 Author URI: https://aleksandarv.com/
-License: GPL2
 */
 
-function display_radnom_quote() {
+function my_admin_notice() {
     global $pagenow;
-    if ($pagenow == 'index.php') {
-        ?>
-        <div id="random-quote"></div>
-        <script src="<?php echo plugins_url( 'index.js', __FILE__ ); ?>"></script>
-        <?php
+    if ($pagenow === 'index.php') {
+        echo '<div id="random-quote-generator-div"><blockquote></blockquote><span>-</span></div>';
     }
 }
-add_action('admin_footer', 'display_radnom_quote');
+add_action('admin_notices', 'my_admin_notice');
+
+function insert_random_quote_generator_indexJS_file($hook) {
+    if ($hook === 'index.php') {
+        wp_enqueue_script('get-random-quote', plugins_url('index.js', __FILE__), array(), false, true);
+    }
+}
+add_action('admin_enqueue_scripts', 'insert_random_quote_generator_indexJS_file');
+
+function enqueue_random_quote_stylesheet() {
+    wp_enqueue_style('random_quote_stylesheet', plugins_url('index.css', __FILE__));
+}
+add_action('admin_enqueue_scripts', 'enqueue_random_quote_stylesheet');
